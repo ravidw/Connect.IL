305218c959bc71a44156bc802b10caaeba66e714
Ravid <ravidj@gmail.com>
2016-01-13 00:22:56 +0200

    First

---
+++ Yonit/includes/js/app.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $('.btn-search').click(function(){
+        var input = $('.search-box input');
+        var form = $('#search-form');
+
+        form.submit(function(event){
+            if(!input.val()){
+                input.addClass('invalid-input');
+                event.preventDefault();
+            }
+
+            return;
+        });
+    });
+
+    $('#search-range').datepicker({
+        inputs: $('.range'),
+        clearBtn: true,
+        startDate: '0'
+    });
+
+    $(function() {
+        $('.readonly-rate-bar').barrating({
+            theme: 'bootstrap-stars',
+            readonly: true
+        });
+    });
+
+    var imgNum = 1;
+    window.setInterval(function(){
+        imgNum == 4 ? imgNum = 1: imgNum++;
+
+        $('#main-image').css('background-image', 'url("includes/images/back' + imgNum + '.jpg")')
+    }, 5000);
+});
---
+++ Yonit/includes/js/details.js
@@ -0,0 +1,30 @@
+function initialize() {
+    var mapProp = {
+        center: new google.maps.LatLng(48.8638118,2.2060195),
+        zoom: 16,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+
+$(document).ready(function() {
+    $(function () {
+        $("#gallery").jGallery({
+            "transition": "moveToLeft_moveFromRight",
+            "transitionBackward": "moveToRight_moveFromLeft",
+            "transitionCols": "1",
+            "transitionRows": "1",
+            "height": "450px",
+            "width": "500px",
+            "thumbHeight": 50,
+            "title": true,
+            "thumbnailsPosition": "bottom",
+            "thumbType": "image",
+            "backgroundColor": "#ebebeb",
+            "browserHistory": false,
+            "textColor": "000000",
+            "mode": "standard"
+        });
+    });
+});
